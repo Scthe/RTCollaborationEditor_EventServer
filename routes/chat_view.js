d4b6f88340d22e7bcfc66a029175c63913240266
Scthe <marcin1113C@gmail.com>
2014-10-01 14:14:16 +0200

    Create better chat view

--- routes/chat_view.js
+++ routes/chat_view.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('chat_view', { title: 'Chat' });
+  res.render('chat_view');
 });
 
 module.exports = router;
