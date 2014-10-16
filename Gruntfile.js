dbdeac451910228a9eb5f6ef8388fe9e95c3c51c
Scthe <marcin1113C@gmail.com>
2014-10-16 17:54:48 +0200

    Add mocha & chai. Use grunt as test runner

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    mochaTest: {
+      specs: {
+        options: {
+          ui: 'bdd',
+          reporter: 'dot',
+          require: './test/helpers/chai',
+          clearRequireCache: true
+        },
+        src: ['test/specs/**/*.js']
+      }
+    },
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js'
+      }
+    },
+    watch: {
+      js: {
+        options: {
+          spawn: false
+        },
+        files: [
+          'test/specs/**/*.js',
+          'server/**/*.js'
+        ],
+        tasks: ['mocha']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['mochaTest', 'watch']);
+  grunt.registerTask('test', ['mochaTest', 'karma']);
+  grunt.registerTask('mocha', ['mochaTest']);
+
+  // On watch events, if the changed file is a test file then configure mochaTest to only
+  // run the tests from that file. Otherwise run all the tests
+  var defaultTestSrc = grunt.config('mochaTest.test.src');
+  grunt.event.on('watch', function(action, filepath) {
+    grunt.config('mochaTest.test.src', defaultTestSrc);
+    if (filepath.match('test/')) {
+      grunt.config('mochaTest.test.src', filepath);
+    }
+  });
+
+};
---
+++ test/specs/test_a.js
@@ -0,0 +1,20 @@
+/*jslint indent: 2 */
+/*jshint expr: true*/
+/* global describe, it, beforeEach, expect, _ */
+
+(function () {
+  'use strict';
+
+  describe('test', function () {
+
+    it('works', function () {
+      expect(true).to.be.true;
+    });
+
+    it('works in an awkward way', function () {
+//      expect(true).to.be.false;
+    });
+
+  });
+
+})();
