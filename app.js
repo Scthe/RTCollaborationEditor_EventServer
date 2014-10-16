6c35511b05385a0ba57cb79b867129be6a6aff9a
Scthe <marcin1113C@gmail.com>
2014-10-16 16:16:03 +0200

    Reorganize files

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var routes = require('./server/routes/index');
 var socket_handler = require('./server/socket_handler');
 var system_events_receiver = require('./server/server_events_receiver');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app-test'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app-test')));
 
 app.use('/', routes);
 
