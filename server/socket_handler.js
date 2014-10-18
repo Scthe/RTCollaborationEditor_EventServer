cb8cc4862d1224d95ca70f57248754b0ee0c134e
Scthe <marcin1113C@gmail.com>
2014-10-18 20:29:43 +0200

    Make SocketHandler more testable

--- server/socket_handler.js
+++ server/socket_handler.js
@@ -9,35 +9,33 @@ module.exports = function (app, port) {
   var server = http.createServer(app).listen(port);
   var io = socketIO(server);
 
-  io.sockets.on('connection', function (socket) {
-    // we have the socket now wait for the message with client data TODO remove this stupid mechanism
-    var client_data;
-    socket.on('client_data', function (msg) {
-      if (!client_data && msg.client_id && msg.chat_room) {
-        client_data = on_socket_connected(app, msg.client_id, msg.chat_room, socket);
+  // create handler for socket connections
+  io.sockets.on('connection', _.partial(onNewConnection, app));
 
-        // disconnect callback
-        socket.on('disconnect', _.partial(on_socket_disconnected, app, client_data));
-
-        // message callback
-        var message_handler = _.partial(on_socket_message, client_data);
-        socket.on('message', message_handler);
-      }
-    });
-  });
+  // give possibility to gracefully close socket server
+  return server;
 };
 
-//region socket lifecycle
-function on_socket_connected(app, client_id, chat_room, socket) {
+function onNewConnection(app, socket) {
   // TODO check if client has r/w rights
-  chat_room = chat_room === 'b' ? 'b' : 'a';
 
   var client_data = {
-    chat_room: chat_room,
-    client_id: client_id
+    client_id: Date.now() % 1000,   // TODO read from socket handshake
+    chat_room: 'aaa'                // TODO read from socket handshake
   };
-  console.log(">>[client_id]" + client_data["client_id"]);
+  console.log("[client_id]" + client_data["client_id"]);
+  on_socket_connected(app, client_data, socket);
 
+  // disconnect callback
+  socket.on('disconnect', _.partial(on_socket_disconnected, app, client_data));
+
+  // message callback
+  var message_handler = _.partial(on_socket_message, client_data);
+  socket.on('message', message_handler);
+}
+
+//region socket lifecycle
+function on_socket_connected(app, client_data, socket) {
   client_data["redis_adapter"] = new RedisAdapter(client_data,
     _.partial(on_message, socket),
     _.partial(on_user_status, socket));
@@ -47,9 +45,7 @@ function on_socket_connected(app, client_id, chat_room, socket) {
 
   // inform the client about the communication parameters
   // ( should be read from handshake cookies really)
-  socket.emit("system", {client_id: client_data["client_id"]});
-
-  return client_data;
+  socket.emit("system", {client_id: client_data["client_id"]}); // TODO remove this
 }
 
 function on_socket_disconnected(app, client_data) {
