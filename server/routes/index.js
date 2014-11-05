ffd453311182437bcb6aa60802d7fe092e86991c
Scthe <marcin1113C@gmail.com>
2014-11-06 00:27:37 +0100

    Add parameter to jade template

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,14 @@ var router = express.Router();
 
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   /* jshint unused:false */ // req is not used
   // TODO read client_id from cookie
   // TODO store the ( client_id, room_id, auth_token)
   // TODO render the page with the token or set is as cache
   // TODO client sends us the token back on socket handshake
   // TODO make token one time use
-  res.render('index');
+  res.render('index', { title: 'Home'});
 });
 
 module.exports = router;
