49f5e4b4acc7d8694f113ae9f85e6c27f414378e
Scthe <marcin1113C@gmail.com>
2014-11-10 18:52:03 +0100

    Create propagation test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,20 @@ module.exports = function (grunt) {
   'use strict';
 
   /*
-  Windows grunt-mocha-casperjs fix:
-  1. open node_modules\grunt-mocha-casperjs\node_modules\.bin\mocha-casperjs.cmd
-  2. replace line 4 with:
-	"%~dp0\..\mocha-casperjs\bin\mocha-casperjs" %*
-  3. open node_modules\grunt-mocha-casperjs\node_modules\mocha-casperjs\bin\mocha-casperjs.bat
-  4. replace last line with:
-	%MOCHA_CASPER_PATH%.\..\..\casperjs\bin\casperjs.exe %MOCHA_CASPER_PATH%\cli.js --mocha-casperjs-path=%MOCHA_CASPER_PATH%.. %*
-  
-  For some reason it was checking if sh was available and if NOT then execute mocha-casperjs through sh. Relative path to casperjs was also wrong.
-  
-  TODO write script to do this.
-  
-  */
-  
+   Windows grunt-mocha-casperjs fix:
+   1. open node_modules\grunt-mocha-casperjs\node_modules\.bin\mocha-casperjs.cmd
+   2. replace line 4 with:
+   "%~dp0\..\mocha-casperjs\bin\mocha-casperjs" %*
+   3. open node_modules\grunt-mocha-casperjs\node_modules\mocha-casperjs\bin\mocha-casperjs.bat
+   4. replace last line with:
+   %MOCHA_CASPER_PATH%.\..\..\casperjs\bin\casperjs.exe %MOCHA_CASPER_PATH%\cli.js --mocha-casperjs-path=%MOCHA_CASPER_PATH%.. %*
+
+   For some reason it was checking if sh was available and if NOT then execute mocha-casperjs through sh. Relative path to casperjs was also wrong.
+
+   TODO write script to do this.
+
+   */
+
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-casperjs');
@@ -46,20 +46,20 @@ module.exports = function (grunt) {
     },
     mocha_casperjs: {
       options: {
-        ui                : 'bdd',
-        reporter          : 'list',
-        require           : ['./test/helpers'],
-        clearRequireCache : true,
-        
-		//'mocha-path'      : 'node_modules/mocha',
+        ui               : 'bdd',
+        reporter         : 'list',
+        require          : ['./test/helpers'],
+        clearRequireCache: true,
+
+        'mocha-path'      : 'node_modules/mocha',
         'chai-path'       : 'node_modules/chai',
         'casper-chai-path': 'node_modules/casper-chai',
         timeout           : 15000,
         casperTimeout     : 14000,
-        
-		slow              : 2000,
-        width             : 1680,
-        height            : 1050
+
+        slow  : 2000,
+        width : 1680,
+        height: 1050
       },
       files  : {
         src: [ 'test/e2e/*.js']
@@ -76,6 +76,7 @@ module.exports = function (grunt) {
 // run the tests from that file. Otherwise run all the tests
   var defaultTestSrc = grunt.config('mochaTest.test.src');
   grunt.event.on('watch', function (action, filepath) {
+    /* jshint unused:false */ // action is not used
     grunt.config('mochaTest.test.src', defaultTestSrc);
     if (filepath.match('test/')) {
       grunt.config('mochaTest.test.src', filepath);
--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -1,10 +1,8 @@
 /*jslint indent: 2 */
 /*jshint expr: true*/
-/* global describe, it,  expect, sinon,  faker, expect */
-/* global casper, before, after */
+/* global casper, it,  expect, faker, beforeEach */
 
 /*
-
  Test plan:
 
  user count:
@@ -39,39 +37,88 @@
  * 1:write
  * expect NO changes
 
+
+
+
+ casper.evaluate(function(username, password) {
+ document.querySelector('#username').value = username;
+ document.querySelector('#password').value = password;
+ document.querySelector('#submit').click();
+ }, 'sheldon.cooper', 'b4z1ng4');
  */
 
 (function () {
   'use strict';
 
+  var url = 'http://localhost:3000/docA';
 
-  describe('e2e', function () {
+  beforeEach(function () {
+    // register phantomjs console handler
+    casper.on('remote.message', function (message) {
+      console.log('>>> ' + message);
+    });
+  });
 
-    it('???', function () {
-      expect(true).to.be.true;
+  it('e2e test 1', function () {
+
+    casper.start(url);
+
+    casper.then(function () {
+      expect('sublime-docs').to.matchTitle;
+
+      // move selector
+      this.evaluate(function () {
+        /*global editor, CodeMirror*/
+        var line = 1,
+            ch = 3,
+            chEnd = 3;
+//            chEnd = 12;
+        var anchor = new CodeMirror.Pos(line, ch),
+            head = new CodeMirror.Pos(line, chEnd),
+            options = {origin: '*mouse'};
+        editor.setSelection(anchor, head, options);
+      });
+
+      casper.capture('../selection.png');
+
     });
 
-    describe('Google searching', function() {
-	  before(function() {
-		casper.start('http://www.google.fr/');
-	  });
+    // write
+    casper.then(function () {
+      // TODO input should be focused ! ( line: 2424, fast bail condition)
+      this.sendKeys('#texteditor-input', 'Test');
+    });
 
-	  it('should retrieve 10 or more results', function() {
-		casper.then(function() {
-		  'Google'.should.matchTitle;
-		  'form[action="/search"]'.should.be.inDOM.and.be.visible;
-		  this.fill('form[action="/search"]', {
-			q: 'casperjs'
-		  }, true);
-		});
+    // wait for event propagation
+    casper.wait(1000);
 
-		casper.waitForUrl(/q=casperjs/, function() {
-		  (/casperjs/).should.matchTitle;
-		});
-	  });
-	});
-
+    // check event result
+    casper.then(function () {
+      var line = this.evaluate(function () {
+        return editor.doc.getLine(1);
+      });
+      expect(line).to.equal('varTest bindings = {');
+    });
 
   });
+
+  /*
+   * snippets:
+   *
+   * LOG CASPER
+   * delete casper.test;
+   * console.log(casper);
+   *
+   * TITLE
+   * var title = this.evaluate(function () {
+   * return document.title;
+   * });
+   *
+   * USER COUNT
+   * var userCount = this.evaluate(function () {
+   * return $('#user_count').text();
+   * });
+   *
+   */
 
 })();
