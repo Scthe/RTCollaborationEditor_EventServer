f17afbb18c5e69d32db019a4440c17a9cf31391b
Scthe <marcin1113C@gmail.com>
2014-11-14 00:36:08 +0100

    Add browser env. error handler snippet

--- test/e2e/e2e.js
+++ test/e2e/e2e.js
@@ -49,6 +49,13 @@
     casper.on('remote.message', function (message) {
       console.log('>>> ' + message);
     });
+
+//    casper.on('page.error', function (msg, trace) {
+//      this.echo('Error:    ' + msg, 'ERROR');
+//      this.echo('file:     ' + trace[0].file, 'WARNING');
+//      this.echo('line:     ' + trace[0].line, 'WARNING');
+//      this.echo('function: ' + trace[0]['function'], 'WARNING');
+//    });
   });
 
   it('Base event propagation test', function () {
