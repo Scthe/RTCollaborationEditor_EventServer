dc31478b064e507b2a07503883e309141a6d3508
Scthe <marcin1113C@gmail.com>
2014-12-02 17:23:56 +0100

    Add jsdocs for server_events_receiver

--- server/server_events_receiver.js
+++ server/server_events_receiver.js
@@ -1,8 +1,7 @@
 'use strict';
 
 /**
- *
- * @type {exports}
+ * @module server/server_events_receiver
  */
 
 var util = require('util');
@@ -12,10 +11,26 @@ module.exports = function (app) {
   app.on('remove user', onRemoveUser);
 };
 
+/**
+ * Function used to inform server about new client
+ * <p/>
+ * Invoked through internal server message bus on channel 'new user'
+ *
+ * @param {ClientData} data data of the new client
+ * @returns *
+ */
 function onNewUser(data) {
   console.info(util.format('[system event] new user: %s', data.clientId));
 }
 
+/**
+ * Function used to inform server about client's departure
+ * <p/>
+ * Invoked through internal server message bus on channel 'remove user'
+ *
+ * @param {ClientData} data data of the new client
+ * @returns *
+ */
 function onRemoveUser(data) {
   console.info(util.format('[system event] remove user: %s', data.clientId));
 }
