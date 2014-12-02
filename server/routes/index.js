449b335f1b5feecf75471172e80909c80e86a477
Scthe <marcin1113C@gmail.com>
2014-12-02 14:50:02 +0100

    Add unnecessary jsdoc for /:docId route

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,14 +5,19 @@ var express = require('express'),
     router = express.Router(),
     jwt = require('jsonwebtoken');
 
-
-/* GET home page. */
+/**
+ * GET home page.
+ *
+ * Express route handler. Used only in testing environment.
+ *
+ * @param {Request} req request object
+ * @param {Stream} res response object
+ */
 router.get('/:id', function (req, res) {
   /* jshint unused:false */ // req is not used
   var data = {
     documentId: req.params.id,
-    clientId  : Date.now() % 1000 // this can be read from cookie
-//    clientId  : 15
+    clientId  : Date.now() % 1000
   };
 
   var token = jwt.sign(data, config.secretKey, { expiresInMinutes: 60 * 5 });
