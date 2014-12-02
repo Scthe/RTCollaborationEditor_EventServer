736d3b3cb970bd03f64004e6cfbc4313a0208011
Scthe <marcin1113C@gmail.com>
2014-12-02 12:40:30 +0100

    Add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
    */
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-casperjs');
 
@@ -64,6 +65,22 @@ module.exports = function (grunt) {
       files  : {
         src: [ 'test/e2e/*.js']
       }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        'app-test/{,*/}*.js',
+        'bin/{,*/}*.js',
+        'server/{,*/}*.js',
+        'test/{,*/}*.js',
+        'utils/{,*/}*.js',
+        '!app-test/javascripts/codemirror.js'
+//        'test/spec/{,*/}*.js'
+      ]
     }
 
   });
